f1e3586a7f9cc7b3b793ffefacaa4e233728c74e
Lautaro Emanuel <emlautarom1@gmail.com>
2025-01-08 10:58:15 -0300

    Remove `And` from `Numeric`

--- Refined/Refined/Numeric.cs
+++ Refined/Refined/Numeric.cs
@@ -11,17 +11,6 @@ public abstract class Add<TLeft, TRight> : IConst<int>
     public static int Value => TLeft.Value + TRight.Value;
 }
 
-public abstract class And<TValue, TLeft, TRight> : IRefinement<TValue>
-    where TLeft : IRefinement<TValue>
-    where TRight : IRefinement<TValue>
-{
-    public static void Refine(TValue value)
-    {
-        TLeft.Refine(value);
-        TRight.Refine(value);
-    }
-}
-
 public abstract class NonZero<TValue> : IRefinement<TValue> where TValue : INumber<TValue>
 {
     public static void Refine(TValue value)
