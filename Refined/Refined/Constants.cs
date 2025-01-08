a4e81963877152e0d0170e4743a2bcd262f42fa2
Lautaro Emanuel <emlautarom1@gmail.com>
2025-01-08 10:55:20 -0300

    Add `0` and `1`

--- Refined/Refined/Constants.cs
+++ Refined/Refined/Constants.cs
@@ -7,6 +7,16 @@ public interface IConst<out T>
     static abstract T Value { get; }
 }
 
+public abstract class _0<T> : IConst<T> where T : INumber<T>
+{
+    public static T Value => T.CreateChecked(0);
+}
+
+public abstract class _1<T> : IConst<T> where T : INumber<T>
+{
+    public static T Value => T.CreateChecked(1);
+}
+
 public abstract class _10<T> : IConst<T> where T : INumber<T>
 {
     public static T Value => T.CreateChecked(10);
